38fdad60f110c2721468ec9b4b2515e7dec0dcf5
TheBoringDude <iamcoderx@gmail.com>
2021-01-18 09:05:53 +0800

    Add purge settings

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
