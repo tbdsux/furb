3c89b78a9489be8ab40d38a07915a8bf4d4e3368
TheBoringDude <iamcoderx@gmail.com>
2021-01-18 12:47:50 +0800

    add config

---
+++ frontend/vite.config.js
@@ -0,0 +1,16 @@
+import vue from '@vitejs/plugin-vue'
+
+const viteEnv = {}
+Object.keys(process.env).forEach((key) => {
+  if (key.startsWith(`VITE_`)) {
+    viteEnv[`import.meta.env.${key}`] = process.env[key]
+  }
+})
+
+export default {
+  alias: {
+    '@': require('path').resolve(__dirname, 'src'),
+  },
+  define: viteEnv,
+  plugins: [vue()],
+}
